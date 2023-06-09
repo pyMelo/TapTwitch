68981bd13dbfe60d5be56b0a759d57490597ea6f
Melo <carmelofuccio22@gmail.com>
2023-06-09 17:37:07 +0200

    implementing : igbd API

--- python/twitchScript.py
+++ python/twitchScript.py
@@ -1,121 +1,182 @@
 import requests
 import json
 from dotenv import load_dotenv
+from igdb_api_python import igdb
 import os
 
-#topgames_url = 'https://api.twitch.tv/helix/games/top'
 
 load_dotenv()
 
 
-client_id_key= os.getenv("client_id")
-client_id_secret= os.getenv("client_secret")
-
-streams_url = 'https://api.twitch.tv/helix/streams?first=100'
-    
+client_id_key = os.getenv("client_id")
+client_id_secret = os.getenv("client_secret")
+code = os.getenv("code")
 
 
-def write_token_to_file(token):
-    with open("token.txt", "w") as file:
+auth_url = "https://id.twitch.tv/oauth2/token?"
+
+
+def write_authtoken_to_file(token):
+    with open("auth_token.txt", "w") as file:
         file.write(token)
 
-def read_token_from_file():
-    with open("token.txt", "r") as file:
+
+def read_authtoken_from_file():
+    with open("auth_token.txt", "r") as file:
         return file.read()
-    
+
+
+def write_clientoken_to_file(token):
+    with open("client_token.txt", "w") as file:
+        file.write(token)
+
+
+def read_clientoken_from_file():
+    with open("client_token.txt", "r") as file:
+        return file.read()
+
+
+# AUTH FOR TWITCH API ////////////////////////////////////////////////
 def auth_token():
-    auth_url = 'https://id.twitch.tv/oauth2/token?'
     auth_param = {
-        'client_id': 'client_id_key',
-        'client_secret': 'client_id_secret',
-        'grant_type': 'authorization_code',
-        'code': '',
-        'redirect_uri': 'http://localhost:3000',
-        'scope' : '',   
+        "client_id": client_id_key,
+        "client_secret": client_id_secret,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": "http://localhost:3000",
+        "scope": "",
     }
 
-    auth_response = requests.post(auth_url,params=auth_param)
-    response_data = (json.loads(auth_response.content.decode()))
-    token = response_data['access_token']
-    expires_date = response_data['expires_in']
+    # TOKEN AUTH ////////////////////////////////////////////////
+    auth_response = requests.post(auth_url, params=auth_param)
+    auth_data = json.loads(auth_response.content.decode())
+    token_auth = auth_data["access_token"]
+    expires_date = auth_data["expires_in"]
     expires_minutes = expires_date // 60
-    print("The token.. : " + token)
-    print("The token expires in.. "  + expires_minutes + " minutes ")
-    write_token_to_file(token)
+    print("The authorization code token.. : " + token_auth)
+    print("The token expires in.. " + str(expires_minutes) + " minutes ")
+
+    write_authtoken_to_file(token_auth)
+
+
+# CLIENT AUTH FOR IGBD API ////////////////////////////////////////////////
+def client_auth():
+    client_param = {
+        "client_id": client_id_key,
+        "client_secret": client_id_secret,
+        "grant_type": "client_credentials",
+        "code": code,
+        "redirect_uri": "http://localhost:3000",
+        "scope": "",
+    }
+
+    # CLIENT AUTH ////////////////////////////////////////////////
+    client_response = requests.post(auth_url, params=client_param)
+    client_data = json.loads(client_response.content.decode())
+    token_client = client_data["access_token"]
+    expires_date = client_data["expires_in"]
+    expires_minutes = expires_date // 60
+    print("The token expires in.. " + str(expires_minutes) + " minutes ")
+
+    print("The client credentials token is.. " + token_client)
+    write_clientoken_to_file(token_client)
+
 
 def taking_datas():
+    streams_url = "https://api.twitch.tv/helix/streams?first=100"
 
-    token = read_token_from_file()
+    auth_token = read_authtoken_from_file()
+    client_token = read_clientoken_from_file()
+
     twitch_headers = {
-        'Authorization':'Bearer '+ token,
-        'Client-ID': 'client_id_key'
+        "Authorization": "Bearer " + auth_token,
+        "Client-ID": client_id_key,
+    }
+
+    igbd_headers = {
+        "Authorization": "Bearer " + client_token,
+        "Client-ID": client_id_key,
     }
 
     stream_id = 0
-    print('Sending data...')
-    response = requests.get(streams_url, headers=twitch_headers)
-    stream_data = json.loads(response.content)
+    print("Sending data...")
 
+    # TWITCH REQUESTS API ////////////////////////////////////////////////
+    twitch_response = requests.get(streams_url, headers=twitch_headers)
+    twitch_data = json.loads(twitch_response.content)
 
+    # CICLE TO STREAM ALL THE DATAS ////////////////////////////////////////////////
+    for stream in twitch_data["data"]:
+        # USER IDENTIFIER TO REFER TO FOR EACH BROADCASTER ///////////////////////////////
+        user_id = stream["user_id"]
 
-    for stream in stream_data['data']:
-            
+        # GETTING INFOS LIKE PROFILE IMAGES AND WHEN IT WAS CREATED
+        user_info_url = f"https://api.twitch.tv/helix/users?id={user_id}"
+        response2 = requests.get(user_info_url, headers=twitch_headers)
+        user_data = json.loads(response2.content)
 
-            user_id = stream['user_id']
-            user_info_url = f'https://api.twitch.tv/helix/users?id={user_id}'
-            response2 = requests.get(user_info_url, headers=twitch_headers)
-            user_data = json.loads(response2.content)
-            
+        # GETTING NUMBER OF FOLLOWERS OF THAT USER
+        follows_url = f"https://api.twitch.tv/helix/users/follows?to_id={user_id}"
+        response3 = requests.get(follows_url, headers=twitch_headers)
+        follows_data = json.loads(response3.content)
 
-            follows_url = f'https://api.twitch.tv/helix/users/follows?to_id={user_id}'
-            response3 = requests.get(follows_url, headers=twitch_headers)
-            follows_data = json.loads(response3.content)
+        del stream["id"]
+        del user_data["data"][0]["id"]
+        game_name = stream["game_name"]
 
-            del stream['id']
-            del user_data['data'][0]['id']
-        
+        igbd_url = f"https://api.igdb.com/v4/games/"
+        igbd_params = {
+            "fields": "name,summary,genres,total_rating,first_release_date,aggregated_rating",
+            "search": game_name,
+        }
 
-            data = {
-                'id': stream_id,
-                'twitch-data': {
-                    'stream':{
-                        'user_id': stream ['user_id'],
-                        'user_name': stream['user_name'],
-                        'game_id': stream['game_id'],
-                        'game_name': stream['game_name'],
-                        'viewer_count': stream['viewer_count'],
-                        'language' : stream['language'],
+        igbd_response = requests.get(igbd_url, headers=igbd_headers, params=igbd_params)
+        games_data = json.loads(igbd_response.content)
+        print(games_data)
+        # IGBD REQUESTS API ////////////////////////////////////////////////
 
-                    },
-                    'user':{
-                        'profile_image_url': user_data['data'][0]['profile_image_url'],
-                        'created_at': user_data['data'][0]['created_at']
-                    }, 
-                    'follower_count': follows_data['total'],
-                }
-            }
-            data_json = json.dumps(data)
+        data = {
+            "id": stream_id,
+            "twitch-data": {
+                "stream": {
+                    "user_id": stream["user_id"],
+                    "user_name": stream["user_name"],
+                    "game_id": stream["game_id"],
+                    "game_name": stream["game_name"],
+                    "viewer_count": stream["viewer_count"],
+                    "language": stream["language"],
+                },
+                "user": {
+                    "profile_image_url": user_data["data"][0]["profile_image_url"],
+                    "created_at": user_data["data"][0]["created_at"],
+                },
+                "follower_count": follows_data["total"],
+            },
+            "igbd-data": games_data,
+        }
+        data_json = json.dumps(data)
 
+        headers_log = {"Content-type": "application/json"}
+        fluent_url = "http://localhost:9090"
+        requests.post(fluent_url, data=data_json, headers=headers_log)
 
-            headers_log = {'Content-type': 'application/json'}
-            fluent_url = 'http://localhost:9090'
-            requests.post(fluent_url, data=data_json, headers=headers_log)
+        stream_id += 1
 
+    print("Data sent successfully.")
 
-            stream_id += 1
-    
-    print('Data sent successfully.')
 
 def main():
-     while True:
+    while True:
         user_input = input("Code been taken? (y/n): ")
         if user_input.lower() == "y":
             print("Sending the api's")
             taking_datas()
 
-        if user_input.lower() == 'n':
+        if user_input.lower() == "n":
             print("Making the auth and then sending api's")
             auth_token()
+            client_auth()
             taking_datas()
+
 
 main()
